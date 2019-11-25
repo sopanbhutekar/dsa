c5f8bc4d77ae504576f1be5e2b52da09ea7d9050
Bhutekar <Sopan.Bhutekar@searshc.com>
2019-11-25 09:32:29 -0600

    Find Longest Common Substring for Array of String

---
+++ LongestCommonSubstring.java
@@ -0,0 +1,17 @@
+package heap;
+import java.io.*;
+
+
+public class LongestCommonSubstring {
+
+	public static void main(String[] args)throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine());
+		for(int i=0;i<t;i++){
+			String str=br.readLine();
+		}
+
+	}
+
+}
